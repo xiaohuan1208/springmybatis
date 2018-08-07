d2fb56679dfe7589b964e595e79d38dcbdec9a68
xiaohuan1208 <1767064404@qq.com>
2018-08-07 09:46:01 +0800

    1.2

--- src/main/java/cn/jxufe/imp/UserServiceImp.java
+++ src/main/java/cn/jxufe/imp/UserServiceImp.java
@@ -8,6 +8,8 @@ import cn.jxufe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by Administrator on 2018/8/6.
  */
@@ -21,20 +23,51 @@ public class UserServiceImp implements UserService {
     @Autowired
     private UserDAO userDAO;
     @Override
-    public Message register(Registerinfo registerinfo) {
+    public Message register(Registerinfo registerinfo, HttpSession session) {
         User user = new User();
         user.setTelphone(registerinfo.getTelphone());
-        user.setPassword("");
-        user.setNickname("sb");
+        user.setPassword("123456");
         Message message = new Message();
-        if(userDAO.insert(user)>0){
+        try {
+            userDAO.insertSelective(user);
             message.setCode(1);
             message.setMessage("注册成功");
-        }else{
+            session.setAttribute("user",user);
+        }catch (Exception e){
             message.setCode(-1);
             message.setMessage("注册失败");
         }
+
         return message;
+    }
+
+    @Override
+    public Message login(User user, HttpSession session) {
+
+        Message message =new Message();
+
+        User user1 =  userDAO.selectByPrimaryKey(user.getTelphone());
+        if(user1 != null){
+            if(user1.getPassword().equals(user.getPassword())){
+                message.setCode(1);
+                message.setMessage("登录成功");
+                session.setAttribute("user",user1);
+            }else{
+                message.setCode(-1);
+                message.setMessage("登录失败！密码错误");
+            }
+        }else{
+            message.setCode(-1);
+            message.setMessage("登录失败!没有该用户！");
+        }
+        return message;
+    }
+
+    @Override
+    public User getUserinfo(HttpSession session) {
+
+        User user = (User)session.getAttribute("user");
+        return user;
     }
 }
 /*
