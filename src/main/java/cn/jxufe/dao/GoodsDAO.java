fc7bf74908ac3ec6d9ef1e0e4148cbd5e1c884de
13687091847 <357066443@qq.com>
2018-08-07 16:28:29 +0800

    本地提交一次

--- src/main/java/cn/jxufe/dao/GoodsDAO.java
+++ src/main/java/cn/jxufe/dao/GoodsDAO.java
@@ -2,11 +2,25 @@ package cn.jxufe.dao;
 
 import cn.jxufe.entity.Goods;
 
+
 import org.springframework.stereotype.Repository;
+
+
+
+
+import java.util.List;
+
 
 /**
  * GoodsDAO继承基类
+ * pppp
  */
 @Repository
 public interface GoodsDAO extends MyBatisBaseDao<Goods, Integer> {
+
+    /**
+     * 通过关键字查询商品信息
+     */
+    public List<Goods> findByName(String goodsName);
+
 }
