bdddebbfb39ef2203a61ade2949b70863bb416aa
xiaohuan1208 <1767064404@qq.com>
2018-08-07 09:45:39 +0800

    1.2

--- src/main/java/cn/jxufe/entity/User.java
+++ src/main/java/cn/jxufe/entity/User.java
@@ -1,17 +1,42 @@
 package cn.jxufe.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
- * T_User
+ * T_UserInfo
  * @author 
  */
 public class User implements Serializable {
+    /**
+     * 手机号（主键）
+     */
     private String telphone;
 
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 昵称
+     */
     private String nickname;
 
-    private String password;
+    /**
+     * 头像
+     */
+    private String headimg;
+
+    /**
+     * 创建时间
+     */
+    private String createtime;
+
+    /**
+     * 修改时间
+     */
+    private String updatetime;
 
     private static final long serialVersionUID = 1L;
 
@@ -23,6 +48,14 @@ public class User implements Serializable {
         this.telphone = telphone;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getNickname() {
         return nickname;
     }
@@ -31,12 +64,28 @@ public class User implements Serializable {
         this.nickname = nickname;
     }
 
-    public String getPassword() {
-        return password;
+    public String getHeadimg() {
+        return headimg;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setHeadimg(String headimg) {
+        this.headimg = headimg;
+    }
+
+    public String getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(String createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(String updatetime) {
+        this.updatetime = updatetime;
     }
 
     @Override
@@ -52,8 +101,11 @@ public class User implements Serializable {
         }
         User other = (User) that;
         return (this.getTelphone() == null ? other.getTelphone() == null : this.getTelphone().equals(other.getTelphone()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()))
             && (this.getNickname() == null ? other.getNickname() == null : this.getNickname().equals(other.getNickname()))
-            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()));
+            && (this.getHeadimg() == null ? other.getHeadimg() == null : this.getHeadimg().equals(other.getHeadimg()))
+            && (this.getCreatetime() == null ? other.getCreatetime() == null : this.getCreatetime().equals(other.getCreatetime()))
+            && (this.getUpdatetime() == null ? other.getUpdatetime() == null : this.getUpdatetime().equals(other.getUpdatetime()));
     }
 
     @Override
@@ -61,8 +113,11 @@ public class User implements Serializable {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((getTelphone() == null) ? 0 : getTelphone().hashCode());
-        result = prime * result + ((getNickname() == null) ? 0 : getNickname().hashCode());
         result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        result = prime * result + ((getNickname() == null) ? 0 : getNickname().hashCode());
+        result = prime * result + ((getHeadimg() == null) ? 0 : getHeadimg().hashCode());
+        result = prime * result + ((getCreatetime() == null) ? 0 : getCreatetime().hashCode());
+        result = prime * result + ((getUpdatetime() == null) ? 0 : getUpdatetime().hashCode());
         return result;
     }
 
@@ -73,8 +128,11 @@ public class User implements Serializable {
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
         sb.append(", telphone=").append(telphone);
-        sb.append(", nickname=").append(nickname);
         sb.append(", password=").append(password);
+        sb.append(", nickname=").append(nickname);
+        sb.append(", headimg=").append(headimg);
+        sb.append(", createtime=").append(createtime);
+        sb.append(", updatetime=").append(updatetime);
         sb.append(", serialVersionUID=").append(serialVersionUID);
         sb.append("]");
         return sb.toString();
