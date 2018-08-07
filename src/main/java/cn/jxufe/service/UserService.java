25a2bd0a2c9dd283bd0e6989667ed659a3168406
xiaohuan1208 <1767064404@qq.com>
2018-08-07 09:47:32 +0800

    1.2

--- src/main/java/cn/jxufe/service/UserService.java
+++ src/main/java/cn/jxufe/service/UserService.java
@@ -4,12 +4,18 @@ import cn.jxufe.bean.Message;
 import cn.jxufe.entity.Registerinfo;
 import cn.jxufe.entity.User;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by Administrator on 2018/8/6.
  */
 public interface UserService {
 
-    public Message register(Registerinfo registerinfo);
+    public Message register(Registerinfo registerinfo, HttpSession session);
+
+    public Message login(User user, HttpSession session);
+
+    public User getUserinfo(HttpSession session);
 }
 /*
 代码手中走~佛祖心中留！求永无BUG！
