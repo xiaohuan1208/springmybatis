ee7952aada511778b5c47993a3c1f7cfbac0e99f
weionly <2959697175@qq.com>
2018-08-09 10:01:53 +0800

    魏德旺

---
+++ src/main/java/cn/jxufe/service/AdvertisementService.java
@@ -0,0 +1,12 @@
+package cn.jxufe.service;
+
+import cn.jxufe.entity.Advertisement;
+
+import java.util.List;
+
+/**
+ * Created by 29596 on 2018/8/8.
+ */
+public interface AdvertisementService {
+    public List<Advertisement> findAll();
+}
