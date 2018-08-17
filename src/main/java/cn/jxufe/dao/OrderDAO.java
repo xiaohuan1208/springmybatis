00e3b3339ae759195259e7ab9db685b2f5aa9f74
13687091847 <357066443@qq.com>
2018-08-17 16:17:13 +0800

    2018.8.17添加了下订单

--- src/main/java/cn/jxufe/dao/OrderDAO.java
+++ src/main/java/cn/jxufe/dao/OrderDAO.java
@@ -1,11 +1,20 @@
 package cn.jxufe.dao;
 
 import cn.jxufe.entity.Order;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * OrderDAO继承基类
  */
 @Repository
-public interface OrderDAO extends MyBatisBaseDao<Order, Integer> {
+public interface OrderDAO extends MyBatisBaseDao<Order, Long> {
+    /**
+     * 查找当前登录用户的未支付订单
+     * @param telphone
+     * @return
+     */
+    public List<Order> findByTelphone(@Param("tel")String telphone,@Param("state")String orderstate);
 }
--- src/main/java/cn/jxufe/dao/OrderdetailsDAO.java
+++ src/main/java/cn/jxufe/dao/OrderdetailsDAO.java
@@ -3,9 +3,13 @@ package cn.jxufe.dao;
 import cn.jxufe.entity.Orderdetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * OrderdetailsDAO继承基类
  */
 @Repository
-public interface OrderdetailsDAO extends MyBatisBaseDao<Orderdetails, String> {
+public interface OrderdetailsDAO extends MyBatisBaseDao<Orderdetails, Integer> {
+    public List<Orderdetails> findByOrderId(long orderId);
+    public int deleteByOrderId(long orderId);
 }
